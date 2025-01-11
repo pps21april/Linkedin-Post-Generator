be96612df7d17032a88140c7f0c6b842ccbedb9f
Prashant Pratap Singh <121862529+pps21april@users.noreply.github.com>
2025-01-11 16:59:45 +0530

    Delete Linkedin_Post_Generator directory

--- Linkedin_Post_Generator/llm_helper.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-from langchain_groq import ChatGroq
-
-load_dotenv()
-
-llm = ChatGroq(model_name = "llama-3.3-70b-versatile" , groq_api_key = os.getenv("GROQ_API_KEY"))
